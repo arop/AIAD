3a051cb151cc6f398aaa8662b9e8c82cb47441e6
João de Almeida <netforum@gmail.com>
2015-12-13 18:30:52 +0000

    run.bat removido

--- run.bat
+++
@@ -1,1 +0,0 @@
-java -cp %JADE_PATH%;out/production/AIAD jade.Boot -agents drHouse:agents.RecursoAgent(raio-x);drHouse1:agents.RecursoAgent(tac);p1:agents.PacienteAgent(5,true,tac,raio-x);p2:agents.PacienteAgent(3,false,tac,raio-x);snif:jade.tools.sniffer.Sniffer -gui
